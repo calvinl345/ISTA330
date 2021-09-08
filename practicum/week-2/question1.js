a6d34449a4c9ea06e938619312933e36e80e6b78
Calvin Smith <calvinl345@gmail.com>
2021-09-08 12:16:24 -0600

    used reduce

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -7,5 +7,5 @@ For example given the input array [5, 6, 1], the running sum is
 
 var runningSum = function (input) {
     console.log("answer");
-    return sum(input);
+    return input.reduce(reducer);
 };
